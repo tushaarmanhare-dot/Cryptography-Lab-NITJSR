9a2e1565bc13bb511a5d2d910b8ac56068f45b4f
tushaarmanhare-dot <tushaarmanhare@gmail.com>
2025-08-31 10:06:56 -0700

    Add files via upload

---
+++ 02_b_Mul_cipher.py
@@ -0,0 +1,52 @@
+from math import gcd
+
+def mod_inverse(a, m=26):
+    for x in range(1,m):
+        if(a * x) % m == 1:
+            print("Inv key: ",x)
+            return x
+    return None
+
+def encrypt(text,shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+
+            result += chr(((ord(char) - base) * shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+def decrypt(text,shift):
+    inv = mod_inverse(shift)
+    if inv is None:
+        return "No inverse exists choose a different key!"
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+
+            result += chr(((ord(char) - base) * inv) % 26 + base)
+        else:
+            result += char
+    return result
+
+msg = input("Enter the message: ")
+key = int(input("Enter the key value(must be coprime with 26): "))
+
+if gcd(key,26) != 1:
+    print("Invalid key! It must be coprime with 26")
+else:
+    enc = encrypt(msg,key)
+    dec = decrypt(enc,key)
+    print("Original message:", msg)
+    print("Encrypted message:", enc)
+    print("Decrypted message:", dec)
+
