e336e6c9fc5d97424c23c5f40602c95da7963fe0
tushaarmanhare-dot <tushaarmanhare@gmail.com>
2025-09-01 12:10:21 -0700

    02_b_Mul_cipher.py

--- 02_b_Mul_cipher.py
+++ 02_b_Mul_cipher.py
@@ -1,14 +1,18 @@
-#Implement Multiplicative Cipher 
-from math import gcd
+#Implement Affine Cipher
+def mod_inverse(a,m):
+    n1, n2 = m, a
+    t1, t2 = 0, 1
+    while n2 != 0:
+        q = n1 // n2
+        n1, n2 = n2, n1 - q*n2
+        t1, t2 = t2, t1 - q*t2
 
-def mod_inverse(a, m=26):
-    for x in range(1,m):
-        if(a * x) % m == 1:
-            print("Inv key: ",x)
-            return x
-    return None
+    if n1 != 1:
+            return None
+    print("Inverse key: ",t1 % m)
+    return t1 % m
 
-def encrypt(text,shift):
+def affine_encrypt(text,a,b,m=26):
     result = ""
     for char in text:
         if char.isalpha():
@@ -16,16 +20,16 @@ def encrypt(text,shift):
                 base = ord('A')
             else:
                 base = ord('a')
-
-            result += chr(((ord(char) - base) * shift) % 26 + base)
+            x = (((a * (ord(char) - base)) + b ) % m)
+            result += chr(x + base)
         else:
             result += char
     return result
 
-def decrypt(text,shift):
-    inv = mod_inverse(shift)
+def affine_decrypt(text,a,b,m=26):
+    inv = mod_inverse(a,m)
     if inv is None:
-        return "No inverse exists choose a different key!"
+        return "Invalid key! It must be coprime with 26."
     result = ""
     for char in text:
         if char.isalpha():
@@ -33,22 +37,20 @@ def decrypt(text,shift):
                 base = ord('A')
             else:
                 base = ord('a')
-
-            result += chr(((ord(char) - base) * inv) % 26 + base)
+            x = ((((ord(char) - base) - b) * inv ) % m)
+            result += chr(x + base)
         else:
             result += char
     return result
 
 msg = input("Enter the message: ")
-key = int(input("Enter the key value(must be coprime with 26): "))
+a = int(input("Enter the multiplicative key: "))
+b = int(input("Enter the additive key: "))
 
-if gcd(key,26) != 1:
-    print("Invalid key! It must be coprime with 26")
-else:
-    enc = encrypt(msg,key)
-    dec = decrypt(enc,key)
-    print("Original message:", msg)
-    print("Encrypted message:", enc)
-    print("Decrypted message:", dec)
+enc = affine_encrypt(msg,a,b)
+dec = affine_decrypt(enc,a,b)
 
-
+print("Original message:",msg)
+print("Encrypted message:",enc)
+print("Decrypted message:",dec)
+          
