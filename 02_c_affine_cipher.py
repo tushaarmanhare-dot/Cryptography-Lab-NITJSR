36bab64772839929a2b6ac9a6bbb758824db79ca
tushaarmanhare-dot <tushaarmanhare@gmail.com>
2025-09-01 01:03:42 -0700

    Add files via upload

---
+++ 02_c_affine_cipher.py
@@ -0,0 +1,56 @@
+#Implement Affine Cipher
+def mod_inverse(a,m):
+    n1, n2 = m, a
+    t1, t2 = 0, 1
+    while n2 != 0:
+        q = n1 // n2
+        n1, n2 = n2, n1 - q*n2
+        t1, t2 = t2, t1 - q*t2
+
+    if n1 != 1:
+            return None
+    print("Inverse key: ",t1 % m)
+    return t1 % m
+
+def affine_encrypt(text,a,b,m=26):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+                x = (((a * (ord(char) - base)) + b ) % m)
+                result += chr(x + base)
+        else:
+            result += char
+    return result
+
+def affine_decrypt(text,a,b,m=26):
+    inv = mod_inverse(a,m)
+    if inv is None:
+        return "Invalid key! It must be coprime with 26."
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+            x = ((((ord(char) - base) - b) * inv ) % m)
+            result += chr(x + base)
+        else:
+            result += char
+    return result
+
+msg = input("Enter the message: ")
+a = int(input("Enter the multiplicative key: "))
+b = int(input("Enter the additive key: "))
+
+enc = affine_encrypt(msg,a,b)
+dec = affine_decrypt(enc,a,b)
+
+print("Original message:",msg)
+print("Encrypted message:",enc)
+print("Decrypted message:",dec)
+          
---
+++ 02_d_Playfair_cipher.py
@@ -0,0 +1,95 @@
+#Implement Playfair Cipher
+def gen_key_matrix(key):
+    key = key.upper().replace("J","I")
+    seen = set()
+    matrix = []
+
+    for char in key:
+        if char.isalpha() and char not in seen:
+            seen.add(char)
+            matrix.append(char)
+
+    for char in "ABCDEFGHIKLMNOPQRSTUVWXYZ":
+        if char not in seen:
+            seen.add(char)
+            matrix.append(char)
+    
+    rows = []
+    for i in range(0,25,5):
+        rows.append(matrix[i:i+5])
+    return rows
+
+def print_matrix(matrix):
+    print("Playfair key matrix: ")
+    for row in matrix:
+        print(" ".join(row))
+    print()
+
+def find_pos(matrix,char):
+    for i,row in enumerate(matrix):
+        if char in row:
+            return i,row.index(char)
+    return None
+
+def playfair_encrypt(text,matrix):
+    text = text.upper().replace("J","I")
+    clean_text = ""
+    for c in text:
+        if c.isalpha():
+            clean_text += c
+    text = clean_text
+
+    digraphs =[]
+    i = 0
+    while i < len(text):
+        a = text[i]
+        b = ""
+        if i+1 < len(text):
+            b = text[i+1]
+        if a == b:
+            digraphs.append(a + "X")
+            i += 1
+        else:
+            if b:
+                digraphs.append(a + b)
+                i += 2
+            else:
+                digraphs.append(a + "X")
+                i += 1
+
+    result = ""
+    for pair in digraphs:
+        r1,c1 = find_pos(matrix,pair[0])
+        r2,c2 = find_pos(matrix,pair[1])
+
+        if r1 == r2:
+            result += matrix[r1][(c1 + 1) % 5]
+            result += matrix[r2][(c2 + 1) % 5]
+
+        elif c1 == c2:
+            result += matrix[(r1 + 1) % 5][c1]
+            result += matrix[(r2 + 1) % 5][c2]
+
+        else:
+            result += matrix[r1][c2]
+            result += matrix[r2][c1]
+
+    return result
+
+key = input("Enter the keyword: ")
+matrix = gen_key_matrix(key)
+print_matrix(matrix)
+
+msg = input("Enter the message to encrypt: ")
+enc = playfair_encrypt(msg,matrix)
+
+print("Original message: ",msg.upper())
+print("Encrypted message: ",enc)
+
+            
+
+
+
+
+
+    
